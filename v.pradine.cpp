6acb8581ae19ac7926d7d22cc24ed31858ec3fbe
Ignas Volodko <ignas.volodko@gmail.com>
2025-02-12 15:15:07 +0200

    Add files via upload
    Pridėtas studentų duomenų įvedimas

--- v.pradine.cpp
+++ v.pradine.cpp
@@ -2,6 +2,25 @@
 #include "studentai.h"
 
 int main() {
-    std::cout << "Studentu galutinio balo skaiciavimo programa\n";
+    Student studentas;
+    int ndSk;
+
+    std::cout << "Iveskite studento varda ir pavarde: ";
+    std::cin >> studentas.vardas >> studentas.pavarde;
+
+    std::cout << "Iveskite atliktu namu darbu skaiciu: ";
+    std::cin >> ndSk;
+
+    std::cout << "Iveskite namu darbu rezultatus: ";
+    for (int i = 0; i < ndSk; ++i) {
+        int paz;
+        std::cin >> paz;
+        studentas.namuDarbai.push_back(paz);
+    }
+
+    std::cout << "Iveskite egzamino rezultata: ";
+    std::cin >> studentas.egzaminas;
+
+    std::cout << "Duomenys sekmingai ivesti!\n";
     return 0;
 }
